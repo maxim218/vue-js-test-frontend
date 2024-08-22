372e7a7de2f5b4e1cdc2e812ad6d9ef8bea9ed8c
maxim218 <maxim.kolotovkin@yandex.ru>
2024-08-22 18:51:16 +0400

    My proxy implementation

---
+++ myOwnProxy.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+//
+// https://learn.javascript.ru/property-accessors#umnye-gettery-settery
+//
+
+function logRefs(a, b) {
+    console.log('A: ' + a.content);
+    console.log('B: ' + b.content);
+}
+
+function myRef(info, name) {
+    let storedInfo = info;
+    return {
+        get content() {
+            return storedInfo;
+        },
+        set content(value) {
+            storedInfo = value;
+            console.log(`Set ${name} to ${storedInfo}`);
+        }
+    };
+}
+
+const a = myRef(100, 'A');
+const b = myRef(400, 'B');
+logRefs(a, b)
+
+a.content = 1111;
+b.content = 4444;
+logRefs(a, b);
